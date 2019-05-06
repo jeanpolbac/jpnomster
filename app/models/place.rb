b2bec013527f71fc6feab9989862f1501747ac11
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-05-06 21:02:28 +0000

    created database for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   validates :name, length: { minimum: 3, too_short: " - %{count} characters is the minimum allowed" }
   validates :address, presence: true
   validates :description, presence: true
+  
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 end
