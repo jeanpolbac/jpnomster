f035c544c5a7f3368501117923afb36c829d93cc
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-05-09 02:03:44 +0000

    fixed wrong region by pushing credentials to heroku again. Still unable to upload pic

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
   config.fog_public = false                       # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    region:                'us-east-2',
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     
