375885351f8ce31d3dbed69a682bb377f81244a8
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-05-09 01:47:25 +0000

    added specific region to aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,10 @@ CarrierWave.configure do |config|
   config.fog_public = false                       # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
+    region:                ENV["us-east-2"]
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region:                ENV["us-east-2"]
+    
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
