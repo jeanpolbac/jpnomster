2fd7a23d0aa01be939c3465ba7e1c67c499e54c7
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2019-05-16 18:55:12 +0000

    added email sent upon adding comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,17 +60,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Configure mailer
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 
   # Teaching mailer to build URL
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
